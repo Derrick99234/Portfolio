41eabdb6a8852dfdec1beadd5e236273e94406ba
Derrick <pshubomi@gmail.com>
2024-03-03 15:09:50 +0100

    fixing the commit conflict

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -9,13 +9,11 @@ window.onscroll = () => {
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
-
+ 
     if (top >= offset && top < offset + height) {
       navLinks.forEach((links) => {
         links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
       });
     }
   });
